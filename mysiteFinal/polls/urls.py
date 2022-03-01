bb9e8725966f4c8697a5e4d6d8920fd6acd54497
Tulio Colares <tulio_saraiva@hotmail.com>
2022-03-01 08:53:42 -0300

    só falta imagem de fundo

--- mysiteFinal/polls/urls.py
+++ mysiteFinal/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name='polls' #Por que colocar essa identificação? Não bastaria contar com a diferenciação do mysite/urls.py não?
+app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
--- mysiteFinal/polls/views.py
+++ mysiteFinal/polls/views.py
@@ -1,31 +1,33 @@
-from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
 
 from .models import Choice, Question
-# Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-    
+
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-    
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
 class DetailView(generic.DetailView):
     model = Question
-    template_model = 'polls/detail.html'
+    template_name = 'polls/detail.html'
+
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/result.html'
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice']) # Aqui ele escolheu usar o método POST mesmo podendo usar GET. Por que?
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) 
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
